7e8f0f5130e52f5ffe3e51b1acd292d728223211
mma-fsa <mr.anderson.1725@gmail.com>
2026-03-26 02:40:54 +0000

    full pipeline

--- ilec_pipeline/src/model/inference.py
+++ ilec_pipeline/src/model/inference.py
@@ -1,34 +1,29 @@
 # Databricks notebook source
 # /// script
 # [tool.databricks.environment]
+# base_environment = "/Workspace/Users/mr.anderson.1725@gmail.com/databricks-ilec/ilec_pipeline/src/model/model_environment.yaml"
 # environment_version = "5"
 # ///
 import mlflow
-
-# COMMAND ----------
-
-model_uri = "models:/workspace.mlops_dev.ilec_pipeline/2"
-req_file = mlflow.pyfunc.get_model_dependencies(
-    model_uri,
-    format="pip",
-)
-%pip install -r "{req_file}"
-dbutils.library.restartPython()
-
-# COMMAND ----------
-
-import mlflow
 from pyspark.sql import functions as F
-model_uri = "models:/workspace.mlops_dev.ilec_pipeline/2"
+
+model_uri = dbutils.widgets.get("model_uri")
+predict_py = mlflow.pyfunc.load_model(model_uri)
 predict_udf = mlflow.pyfunc.spark_udf(spark, model_uri, env_manager="local")
 
 # COMMAND ----------
 
-catalog = "workspace"
-schema = "mlops_dev"
+input_cols = getattr(predict_py.unwrap_python_model(), "INPUT_COLS")
+input_cols
+
+# COMMAND ----------
+
+catalog = dbutils.widgets.get("catalog")
+input_schema = dbutils.widgets.get("input_schema")
+input_table = dbutils.widgets.get("input_table")
 
 tbl_ilec_data = (
-    spark.read.table(f"{catalog}.{schema}.ilec_data")
+    spark.read.table(f"{catalog}.{input_schema}.{input_table}")
     .filter(
         (F.trim(F.col("Insurance_Plan")) == "TERM") &
         (F.col("SOA_Post_Lvl_Ind") != F.lit("PLT")) &
@@ -42,43 +37,31 @@ tbl_ilec_data = (
 
 # COMMAND ----------
 
+output_schema = dbutils.widgets.get("output_schema")
+output_table = dbutils.widgets.get("output_table")
 
-tbl_w_preds = tbl_ilec_data.repartition(100).withColumn(
+tbl_w_preds = tbl_ilec_data.withColumn(
         "prediction",
         predict_udf(
-            F.struct(
-                "Sex",
-                "Smoker_Status",
-                "Face_Amount_Band",
-                "Attained_Age",
-                "ExpDth_VBT2015wMI_Cnt",
-            )
-        ),
+            F.struct(*input_cols)
+        )
     )
 
+output_table_full_name = f"{catalog}.{output_schema}.{output_table}"
 (
     tbl_w_preds
     .write
     .mode("overwrite")
-    .saveAsTable("workspace.mlops_dev.term_preds")
+    .saveAsTable(output_table_full_name)
 )
 
 
 
 # COMMAND ----------
 
-tbl_check.limit(100).toPandas()
-
-# COMMAND ----------
-
-tbl_check = spark.table("workspace.mlops_dev.term_preds")
-
+tbl_check = spark.table(output_table_full_name)
 (
     tbl_check
     .withColumn("pred_cnt", F.col("prediction") * F.col("ExpDth_VBT2015wMI_Cnt"))
 ).agg(F.sum(F.col("pred_cnt")), F.sum(F.col("Death_Count"))).toPandas() 
-
-
-# COMMAND ----------
-
 
